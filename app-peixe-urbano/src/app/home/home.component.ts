d87ed2326dfb903038390c25d7f5f7b9591cb8d4
maicom-yamakishi <maicom.yamakishi@gmail.com>
2024-08-30 15:41:43 -0300

    app-peixe-urbano added ofertasService and ngFor in home html

--- app-peixe-urbano/src/app/home/home.component.ts
+++ app-peixe-urbano/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OfertasService } from "app/ofertas.service";
+import { Oferta } from "app/shared/oferta.model";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+  providers: [OfertasService],
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Oferta[];
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) {}
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas);
   }
-
 }
